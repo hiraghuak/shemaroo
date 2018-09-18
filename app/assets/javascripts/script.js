ecfd1159cfbba0f77eb47f98353772c6ca6a76f7
ksamy <karuppasamy@carmatec.in>
2018-09-18 19:03:14 +0530

    close menu in mobile and gradient

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
- 
+	$('.close-hambergmenu img').click(function() {
+		//$('.fixed-top .navbar-collapse').removeClass('show');	
+		/*$('.fixed-top .navbar-collapse').slideUp(200);	
+		$('.navbar-toggler').attr("aria-expanded","false");*/
+		$('.fixed-top .navbar-collapse').removeClass('show');	
+	});
 });
