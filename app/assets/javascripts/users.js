8d55b56ed1093cdb3b000cd1ebd2d203316cb1b7
Kotesh <kotesh@saranyu.in>
2018-09-28 12:12:09 +0530

    all tvshows issues

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
  $("#user_register").click(function(){
  	var user_name = $("#user_name").val();
- 	var mobile_no = $("#mobile_no").val();
+ 	var mobile_no = $("#mobile_number").val();
  	var pwd = $("#password").val();
  	var cf_pwd = $("#confirm_password").val();
 	$.ajax({
-		url: "/users/sign_in",
+		url: "/users/sign_up",
 		type: "POST",
 		data: { 
 		name: user_name, 
 		mobileno: mobile_no,
-		password: pwd,
-		confirm_password: cf_pwd
+		password: pwd
 		},
 		success: function(response,status){
+			console.log("sadsaasds")
 		}
 	});
  })
