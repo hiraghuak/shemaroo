b4fd0e99d7ecb16331749b23e841657e93fe064d
Kotesh <kotesh@saranyu.in>
2018-10-05 15:12:38 +0530

    play url fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
- skip_before_action  :verify_authenticity_token
+ #skip_before_action  :verify_authenticity_token
  def sign_up
 	 begin
 	  signup_params = {
