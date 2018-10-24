44b21f8bfb3e1de1b3061ec46b5770ad412643e5
Ramana <ramana.ala07@gmail.com>
2018-10-24 11:36:26 +0530

    Payment urls

--- config/routes.rb
+++ config/routes.rb
@@ -64,11 +64,11 @@ Rails.application.routes.draw do
   post '/payment/payment_canceled' => 'plans#payment_canceled'
   post '/payment/m_payment_canceled' => 'plans#mobile_payment_canceled'
  
-  # post '/payment/payment_response' => 'plans#payment_response'
-  # post '/payment/m_payment_response' => 'plans#mobile_payment_response'
+  post '/payment/payment_response' => 'plans#payment_response'
+  post '/payment/m_payment_response' => 'plans#mobile_payment_response'
  
-  match 'payment/payment_response' => 'plans#payment_response', via: [:get, :post]
-  match 'payment/m_payment_response' => 'plans##mobile_payment_response', via: [:get, :post]
+  # match 'payment/payment_response' => 'plans#payment_response', via: [:get, :post]
+  # match 'payment/m_payment_response' => 'plans##mobile_payment_response', via: [:get, :post]
  
   get '/plans/apply_promocode' => 'plans#apply_promocode'
   # get '/plans/m_apply_promocode' => 'plans##mobile_apply_promocode'
