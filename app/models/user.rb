ae6c0444b4611ca5b2c4fe5dd177bbbc0c42bfe5
Kotesh <kotesh@saranyu.in>
2018-10-29 18:50:15 +0530

    fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -56,7 +56,7 @@ class User
     end
 
     def self.activate_tv_code(code_params)
-      HTTP.post "verify_tv_token?token=#{code}&region=#{$region}",code_params,"user"
+      HTTP.post "generate_session_tv?token=#{code}&region=#{$region}",code_params,"user"
     end
 
 end
